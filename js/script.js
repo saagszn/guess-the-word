2c3f5d31f9ccf1e08a961525ca0383887c5bd43b
aeneas_h <ashemp90@gmail.com>
2021-05-25 22:07:27 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessLetterButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const resmainingGuessesSpan = document.querySelector(".remaining-span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+
+const word = "magnolia"
+
+const hiddenLetters = function (word) {
+    const hiddenLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        hiddenLetters.push("");
+    }
+    wordInProgress.innerText = hiddenLetters.join("");
+};
+
+placeholder(word);
+
+guessLetterButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    const guess = letterInput.value;
+    console.log(guess);
+    letterInput.value = "";
+});
